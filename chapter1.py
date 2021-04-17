48e7cc5973830430b6c9d00240c337c8df59c987
rohan <rohansadawarte43@gmail.com>
2021-04-17 21:48:06 +0530

    removed comments

--- chapter1.py
+++ chapter1.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 #Read image
-'''
+
 print("package imported")
 
 img = cv2.imread("Resources/nyny.jfif")
 
 cv2.imshow("output",img)
 cv2.waitKey(0)
-'''
+
 
 #Read Video
 '''cap = cv2.VideoCapture("Resources/q1.mkv")
@@ -21,7 +21,7 @@ while True :
         break'''
 
 #Read Webcam
-'''
+
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)  
@@ -32,7 +32,7 @@ while True :
     cv2.imshow("Cam",img)
     if cv2.waitKey(10) & 0xFF ==ord('q'):
         break
-'''
+
 
 
 
